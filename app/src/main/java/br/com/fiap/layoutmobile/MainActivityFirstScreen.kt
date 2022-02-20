f1fa6c94aa168958308fc7841b6f3c5f53c1bc08
daianefernandes <daianedsdaia@gmail.com>
2022-02-20 19:09:10 -0300

    publicando arquivos

---
+++ app/src/main/java/br/com/fiap/layoutmobile/MainActivityFirstScreen.kt
@@ -0,0 +1,11 @@
+package br.com.fiap.layoutmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityFirstScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_screen)
+    }
+}
